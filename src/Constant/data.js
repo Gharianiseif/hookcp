9e2e16c0e0554bd4821367c64fb6d493d17f3627
seif <HP@DL-LP-019.dlines.local>
2021-04-18 17:51:53 +0100

    hooks

---
+++ src/Constant/data.js
@@ -0,0 +1,50 @@
+export default [
+    {
+      id:1,
+    title: "CÉLIBATAIRES",
+    description:
+      "  Trois amis font le serment de rester célibataire. Mais l'un d'entre eux va tomber amoureux.",
+    posterURL:
+      "https://fr.web.img3.acsta.net/medias/nmedia/18/36/02/60/18478319.jpg", 
+    rating: "5",
+    trailerPage: 
+      "https://www.youtube.com/watch?v=zWM7nYIptwA",
+  },
+  {
+      id:2,
+    title: "Première Année de Fac",
+    description:
+      " Antoine entame sa première année de médecine pour la troisième fois. Benjamin arrive directement du lycée, mais il réalise rapidement que cette année ne sera pas une promenade de santé. Dans un environnement compétitif violent, avec des journées de cours ardues et des nuits dédiées aux révisions plutôt qu'à la fête, les deux étudiants devront s’acharner et trouver un juste équilibre entre les épreuves d’aujourd’hui et les espérances de demain.",
+    posterURL:
+      "https://imgr.cineserie.com/2018/11/avant-4.jpg?imgeng=/f_jpg/cmpr_0/w_660/h_370/m_cropbox&ver=1",
+    rating: "1",
+    trailerPage: 
+      "https://www.youtube.com/watch?v=iIaAC2bRthE",
+
+  },
+  {
+      id:3,
+    title: "PocahUne Nouvelle Vie",
+    description:
+      "  Après la mort de sa mère, une femme rencontre sa demi-soeur en cherchant à connaître son père.",
+    posterURL:
+    "https://fr.web.img6.acsta.net/c_310_420/pictures/14/08/22/12/03/314343.jpg",
+    rating: "2",
+    trailerPage: 
+      "https://www.youtube.com/watch?v=dJ6e-rlmtCY",
+
+  },
+  {
+      id:4,
+    title: "FROZMulan, la guerrière légendaire",
+    description:
+      " Mulan est une jeune chinoise, fille de bonne famille. Quand son père malade est appelée sur le champ de bataille après l'invasion des Huns, la jeune fille refuse de le voir prendre les armes. Elle se fait alors passer pour un homme et devient une courageuse guerrière au service de sa nation.",
+    posterURL:
+    "https://fr.web.img6.acsta.net/medias/nmedia/18/84/32/61/19744921.jpg",
+    rating: "3",
+    trailerPage: 
+      "https://www.youtube.com/watch?v=eNwozWTZk6s",
+
+  },
+
+  ];
